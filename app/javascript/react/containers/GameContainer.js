8cdfc9faaad6eef6a62a90dce87b58d9e419076c
James Hartman <hartman.james.p@gmail.com>
2018-03-04 15:31:47 -0500

    added channel test

--- app/javascript/react/containers/GameContainer.js
+++ app/javascript/react/containers/GameContainer.js
@@ -10,18 +10,18 @@ class GameContainer extends Component {
 
   componentDidMount() {
     App.gameChannel = App.cable.subscriptions.create(
-  {
-    channel: "GameChannel",
-    game_id: 1 //this.props.gameId
-  },
-  {
-    connected: () => console.log("GameChannel connected"),
-    disconnected: () => console.log("GameChannel disconnected"),
-    received: data => {
-      console.log(data)
-    }
-  }
-);
+      {
+        channel: "GameChannel",
+        game_id: 1 //this.props.gameId
+      },
+      {
+        connected: () => console.log("GameChannel connected"),
+        disconnected: () => console.log("GameChannel disconnected"),
+        received: data => {
+          console.log(data)
+        }
+      }
+    );
   }
 
   render() {
